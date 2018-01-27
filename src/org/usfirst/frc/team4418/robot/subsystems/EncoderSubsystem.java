76b290384d0b0e9e0c6c09bd1c89d71a4bcdc01f
Shaun Keys <mariobuddy@gmail.com>
2018-01-26 19:34:17 -0700

    Encoder checkpoint

--- src/org/usfirst/frc/team4418/robot/subsystems/EncoderSubsystem.java
+++ src/org/usfirst/frc/team4418/robot/subsystems/EncoderSubsystem.java
@@ -1,16 +1,30 @@
 package org.usfirst.frc.team4418.robot.subsystems;
 
+import org.usfirst.frc.team4418.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
- *
+ * Do not require this subsystem! We wish to report values to shuffleboard and link the encoder properties to the gear shifts so that they are always accurate
  */
 public class EncoderSubsystem extends Subsystem {
 
+	Encoder leftEnc = new Encoder(RobotMap.leftEncoderA,RobotMap.leftEncoderB);
+	Encoder rightEnc = new Encoder(RobotMap.rightEncoderA,RobotMap.rightEncoderB,true);
+	
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 	public EncoderSubsystem() {
 		
+	}
+	
+	private static void setProps(bool isHighGear) {
+		float highGearRatio = 9.07;
+		float lowGearRatio = 19.61;
+		if(isHighGear) {
+			
+		}
 	}
 
     public void initDefaultCommand() {
